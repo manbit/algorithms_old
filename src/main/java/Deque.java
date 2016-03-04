a4e8c54381c24ebfbae9beee554d9b94db477a67
Serhii Ziniuk <serhii_ziniuk@epam.com>
2016-03-04 18:27:21 +0200

    w2_deque_v.0

---
+++ src/main/java/Deque.java
@@ -0,0 +1,140 @@
+import edu.princeton.cs.introcs.StdOut;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import static edu.princeton.cs.introcs.StdOut.print;
+
+public class Deque<Item> implements Iterable<Item> {
+
+    private Node head, tail;
+    private int size;
+
+    public Deque() {
+        head = new Node();
+        tail = new Node();
+        head.next = tail;
+        tail.previous = head;
+    }                           // construct an empty deque
+
+    public static void main(String[] args) {
+        Deque<String> deque = new Deque<String>();
+        deque.addFirst("First");
+        deque.addLast("Last");
+        deque.addFirst("First-1");
+        deque.addLast("Last-1");
+        String item = deque.removeFirst();
+        print(item);
+        item = deque.removeLast();
+        print(item);
+        Iterator<String> iterator = deque.iterator();
+        while (iterator.hasNext()) {
+            print(iterator.next());
+            iterator.remove();
+        }
+        while (iterator.hasNext()) {
+            print(iterator.next());
+        }
+    }   // unit testing
+
+    public boolean isEmpty() {
+        return size == 0;
+    }                 // is the deque empty?
+
+    public int size() {
+        return size;
+    }                        // return the number of items on the deque
+
+    public void addFirst(Item item) {
+        if (item == null)
+            throw new NullPointerException();
+
+        if (head.value != null) {
+            Node newHead = new Node();
+            newHead.value = item;
+            head.previous = newHead;
+            newHead.next = head;
+            head = newHead;
+        } else {
+            head.value = item;
+        }
+        size++;
+    }          // add the item to the front
+
+    public void addLast(Item item) {
+        if (item == null)
+            throw new NullPointerException();
+
+        if (tail.value != null) {
+            Node newTail = new Node();
+            newTail.value = item;
+            tail.next = newTail;
+            newTail.previous = tail;
+            tail = newTail;
+        } else {
+            tail.value = item;
+        }
+        size++;
+    }           // add the item to the end
+
+    public Item removeFirst() {
+        if (size == 0) {
+            throw new NoSuchElementException();
+        }
+        Item removingItem = null;
+        if (head.value != null) {
+            removingItem = head.value;
+            head = head.next;
+            head.previous = null;
+        }
+        size--;
+        return removingItem;
+    }                // remove and return the item from the front
+
+    public Item removeLast() {
+        if (size == 0) {
+            throw new NoSuchElementException();
+        }
+        Item removingItem = null;
+        if (tail.value != null) {
+            removingItem = tail.value;
+            tail = tail.previous;
+            tail.next = null;
+        }
+        size--;
+        return removingItem;
+    }                 // remove and return the item from the end
+
+    public Iterator<Item> iterator() {
+        return new DequeIterator<Item>();
+    }         // return an iterator over items in order from front to end
+
+    public class DequeIterator<Item> implements Iterator<Item> {
+
+        private Node current;
+
+        public DequeIterator() {
+            this.current = head;
+        }
+
+        public boolean hasNext() {
+            return current.next != null;
+        }
+
+        public Item next() {
+            Item next = (Item) current.value;
+            current = current.next;
+            return next;
+        }
+
+        public void remove() {
+            current = current.next;
+        }
+    }
+
+    private class Node {
+        Item value;
+        Node next;
+        Node previous;
+    }
+}
--- src/main/java/Percolation.java
+++ src/main/java/Percolation.java
@@ -10,14 +10,19 @@ public class Percolation {
 
     // create N-by-N grid, with all sites blocked
     public Percolation(int N) {
-        if(N <= 0) {
-            throw new IllegalArgumentException(String.format("N %d",N));
+        if (N <= 0) {
+            throw new IllegalArgumentException(String.format("N %d", N));
         }
         this.N = N;
         sites = new int[N * N + 2];
         wquf = new WeightedQuickUnionUF(N * N + 2);
         sites[0] = 1;
-        sites[sites.length -1 ] = 1;
+        sites[sites.length - 1] = 1;
+    }
+
+    // test client (optional)
+    public static void main(String[] args) {
+
     }
 
     // open site (row row, column column) if it is not open already
@@ -41,19 +46,19 @@ public class Percolation {
             wquf.union(index, sites.length - 1);
         }
 
-//        if the left site is open - union them
+        //        if the left site is open - union them
         if (column > 1 && isOpen(row, column - 1)) {
             wquf.union(index, getIndex(row, column - 1));
         }
-//        if the top site is open - union them
+        //        if the top site is open - union them
         if (row > 1 && isOpen(row - 1, column)) {
             wquf.union(index, getIndex(row - 1, column));
         }
-//        if the right site is open - union them
+        //        if the right site is open - union them
         if (column < N && isOpen(row, column + 1)) {
             wquf.union(index, getIndex(row, column + 1));
         }
-//        if the bottom site is open - union them
+        //        if the bottom site is open - union them
         if (row < N && isOpen((row + 1), column)) {
             wquf.union(index, getIndex(row + 1, column));
         }
@@ -69,28 +74,23 @@ public class Percolation {
 
     // is site (row row, column column) full?
     public boolean isFull(int row, int column) {
-        if(!isValid(row, column)) {
+        if (!isValid(row, column)) {
             throw new IndexOutOfBoundsException(String.format("Row %d, column %d", row, column));
         }
 
-        return this.wquf.connected(0, getIndex(row,column));
+        return this.wquf.connected(0, getIndex(row, column));
     }
 
     // does the system percolate?
     public boolean percolates() {
-        return wquf.connected(0, N*N + 1);
+        return wquf.connected(0, N * N + 1);
     }
 
     private boolean isValid(int row, int column) {
-        return row > 0 && row <= N*N && column > 0 && column <= N*N;
+        return row > 0 && row <= N * N && column > 0 && column <= N * N;
     }
 
     private int getIndex(int row, int column) {
-        return (row - 1)  * N + column;
-    }
-
-    // test client (optional)
-    public static void main(String[] args) {
-
+        return (row - 1) * N + column;
     }
 }
